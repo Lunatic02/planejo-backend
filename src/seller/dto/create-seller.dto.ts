719d3dfcf9639db46b19f663dc45162ac9d672de
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-10-10 16:59:45 -0300

    Error handling

--- src/seller/dto/create-seller.dto.ts
+++ src/seller/dto/create-seller.dto.ts
@@ -1,5 +1,11 @@
+import { MaxLength, IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateSellerDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
   email: string;
   cellphone: string;
   cep: string;
