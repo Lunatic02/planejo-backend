e641760d3705df46720f07265d00df229d7659f6
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-10-11 19:41:07 -0300

    DTO sell

--- src/sells/dto/create-sell.dto.ts
+++ src/sells/dto/create-sell.dto.ts
@@ -1,1 +1,10 @@
-export class CreateSellDto {}
+export class CreateSellDto {
+  amount: number;
+  paymentType: string;
+  products: JSON;
+  order: boolean;
+  supplier?: string
+  deliveryDate DateTime?
+  clientId Int
+  sellerId Int
+}
